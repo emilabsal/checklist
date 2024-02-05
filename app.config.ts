22a1b3ae97de3dd08793b0b279d7f5607e91a84e
Emil Absalyamov <emil-absalyam.off@yandex.ru>
2024-02-05 22:59:05 +0300

    changes

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,6 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docs',
-    description: 'The best place to start your documentation.',
+    title: 'Checklist',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
@@ -33,6 +32,12 @@ export default defineAppConfig({
       showLinkIcon: true,
       exclude: [],
       fluid: true
+    },
+    footer: {
+      creadits: {
+        icon: false,
+        text: 'hello'
+      }
     }
   }
 })
